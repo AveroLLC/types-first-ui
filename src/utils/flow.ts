fa6ed64aa3284de0e3513adb4a0d8f2399e4d69f
Kevin Saldana <ksaldana@averoinc.com>
2018-08-08 10:13:08 -0600

    Cleanup typings for flow definition

--- src/utils/flow.ts
+++ src/utils/flow.ts
@@ -17,78 +17,8 @@
 import { flow as fpFlow } from 'lodash/fp';
 import { StateTransform } from '../types';
 
-export function flow<T extends object>(set1: StateTransform<T>): (state: T) => T;
 export function flow<T extends object>(
-  set1: StateTransform<T>,
-  set2: StateTransform<T>
-): (state: T) => T;
-export function flow<T extends object>(
-  set1: StateTransform<T>,
-  set2: StateTransform<T>,
-  set3: StateTransform<T>
-): (state: T) => T;
-export function flow<T extends object>(
-  set1: StateTransform<T>,
-  set2: StateTransform<T>,
-  set3: StateTransform<T>,
-  set4: StateTransform<T>
-): (state: T) => T;
-export function flow<T extends object>(
-  set1: StateTransform<T>,
-  set2: StateTransform<T>,
-  set3: StateTransform<T>,
-  set4: StateTransform<T>,
-  set5: StateTransform<T>
-): (state: T) => T;
-export function flow<T extends object>(
-  set1: StateTransform<T>,
-  set2: StateTransform<T>,
-  set3: StateTransform<T>,
-  set4: StateTransform<T>,
-  set5: StateTransform<T>,
-  set6: StateTransform<T>
-): (state: T) => T;
-export function flow<T extends object>(
-  set1: StateTransform<T>,
-  set2: StateTransform<T>,
-  set3: StateTransform<T>,
-  set4: StateTransform<T>,
-  set5: StateTransform<T>,
-  set6: StateTransform<T>,
-  set7: StateTransform<T>
-): (state: T) => T;
-export function flow<T extends object>(
-  set1: StateTransform<T>,
-  set2: StateTransform<T>,
-  set3: StateTransform<T>,
-  set4: StateTransform<T>,
-  set5: StateTransform<T>,
-  set6: StateTransform<T>,
-  set7: StateTransform<T>,
-  set8: StateTransform<T>
-): (state: T) => T;
-export function flow<T extends object>(
-  set1: StateTransform<T>,
-  set2: StateTransform<T>,
-  set3: StateTransform<T>,
-  set4: StateTransform<T>,
-  set5: StateTransform<T>,
-  set6: StateTransform<T>,
-  set7: StateTransform<T>,
-  set8: StateTransform<T>,
-  set9: StateTransform<T>
-): (state: T) => T;
-export function flow<T extends object>(
-  set1: StateTransform<T>,
-  set2: StateTransform<T>,
-  set3: StateTransform<T>,
-  set4: StateTransform<T>,
-  set5: StateTransform<T>,
-  set6: StateTransform<T>,
-  set7: StateTransform<T>,
-  set8: StateTransform<T>,
-  set9: StateTransform<T>,
-  set10: StateTransform<T>
+  ...transforms: StateTransform<T>[]
 ): (state: T) => T;
 
 export function flow<T extends object>(...args: any[]): (state: T) => T {
